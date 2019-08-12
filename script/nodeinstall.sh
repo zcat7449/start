cb58fe275ab06e5363a79ee8f1e1f6a43d84d7e6
zcat7449 <komtehsnab@gmail.com>
2019-08-12 16:10:28 +0500

    Update nodeinstall.sh

--- script/nodeinstall.sh
+++ script/nodeinstall.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sed '10i\192.168.1.220 kub-master1.lan kub-master1' /etc/hosts
-sed '11i\192.168.1.221 kub-master2.lan kub-master2' /etc/hosts
-sed '12i\192.168.1.222 knode1.lan knode1' /etc/hosts
+sed '10i\192.168.1.221 kmaster01.lan kub-master01' /etc/hosts
+sed '11i\192.168.1.222 kmaster02.lan kub-master02' /etc/hosts
+sed '12i\192.168.1.223 knode03.lan knode03' /etc/hosts
+sed '12i\192.168.1.224 knode04.lan knode04' /etc/hosts
+sed '12i\192.168.1.225 knode05.lan knode05' /etc/hosts
 
 apt update
 apt install -y docker.io
