9c5c30f6180c40b7c1da278e25edabfc1b086327
zcat7449 <komtehsnab@gmail.com>
2019-08-12 17:02:04 +0500

    Update create-config.sh

--- script/create-config.sh
+++ script/create-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # local machine ip address
-export K8SHA_IPLOCAL=172.26.133.21
+export K8SHA_IPLOCAL=192.168.1.221
 
 # local machine etcd name, options: etcd1, etcd2, etcd3, etcd4, etcd5
 export K8SHA_ETCDNAME=etcd1
@@ -13,47 +13,47 @@ export K8SHA_KA_STATE=MASTER
 export K8SHA_KA_PRIO=102
 
 # local machine keepalived network interface name config, for example: eth0
-export K8SHA_KA_INTF=ens18
+export K8SHA_KA_INTF=eth0
 
 #######################################
 # all masters settings below must be same
 #######################################
 
 # master keepalived virtual ip address
-export K8SHA_IPVIRTUAL=172.26.133.20
+export K8SHA_IPVIRTUAL=192.168.1.220
 
 # master01 ip address
-export K8SHA_IP1=172.26.133.21
+export K8SHA_IP1=192.168.1.221
 
 # master02 ip address
-export K8SHA_IP2=172.26.133.22
+export K8SHA_IP2=192.168.1.222
 
 # master03 ip address
-export K8SHA_IP3=172.26.133.23
+#export K8SHA_IP3=172.26.133.23
 
 # master04 ip address
-export K8SHA_IP4=172.26.133.24
+#export K8SHA_IP4=172.26.133.24
 
 # master05 ip address
-export K8SHA_IP5=172.26.133.25
+#export K8SHA_IP5=172.26.133.25
 
 # master01 hostname
-export K8SHA_HOSTNAME1=hb-master01
+export K8SHA_HOSTNAME1=kmaster01
 
 # master02 hostname
-export K8SHA_HOSTNAME2=hb-master02
+export K8SHA_HOSTNAME2=kmaster02
 
 # master03 hostname
-export K8SHA_HOSTNAME3=hb-master03
+#export K8SHA_HOSTNAME3=hb-master03
 
 # master04 hostname
-export K8SHA_HOSTNAME4=hb-master04
+#export K8SHA_HOSTNAME4=hb-master04
 
 # master04 hostname
-export K8SHA_HOSTNAME4=hb-master05
+#export K8SHA_HOSTNAME4=hb-master05
 
 # keepalived auth_pass config, all masters must be same
-export K8SHA_KA_AUTH=55df7dc334c90194d1600c483e10acfr
+export K8SHA_KA_AUTH=55df7dc334c90194d1600c483e10acfr111
 
 # kubernetes cluster token, you can use 'kubeadm token generate' to get a new one
 export K8SHA_TOKEN=4ae6cb.9dbc7b3600a3de89
