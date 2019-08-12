a156ebae1a4bcebc8e2be24d60b263117d05aebc
zcat7449 <komtehsnab@gmail.com>
2019-08-12 16:35:23 +0500

    Update nodeinstall.sh

--- script/nodeinstall.sh
+++ script/nodeinstall.sh
@@ -4,7 +4,7 @@ sed -i '1i\192.168.1.221 kmaster01.lan kub-master01' /etc/hosts
 sed -i '2i\192.168.1.222 kmaster02.lan kub-master02' /etc/hosts
 sed -i '3i\192.168.1.223 knode03.lan knode03' /etc/hosts
 sed -i '4i\192.168.1.224 knode04.lan knode04' /etc/hosts
-sed -i  knode05.lan knode05' /etc/hosts
+sed -i '5i\192.168.1.225 knode05.lan knode05' /etc/hosts
 echo "Hosts файл обновлен!!!!!!!"
 
 apt update
