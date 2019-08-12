909730d406728d35af2e9ee9f3c0db42d521459f
zcat7449 <komtehsnab@gmail.com>
2019-08-12 16:55:57 +0500

    Update nodeinstall.sh

--- script/nodeinstall.sh
+++ script/nodeinstall.sh
@@ -11,8 +11,7 @@ apt update
 apt install -y docker.io
 apt update && apt install -y apt-transport-https curl 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 
-cat << EOF> /etc/apt/sources.list.d/kubernetes.list 
-deb http://apt.kubernetes.io/ kubernetes-xenial main 
-EOF 
+sed -i '1i\deb http://apt.kubernetes.io/ kubernetes-xenial main' /etc/apt/sources.list.d/kubernetes.list 
+  
 apt update
 apt install -y kubelet kubeadm kubectl
