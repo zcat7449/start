eb41219f3cf40743ce0d8972036d79db8d1a9646
zcat7449 <komtehsnab@gmail.com>
2019-08-12 17:46:30 +0500

    Update nodeinstall.sh

--- script/nodeinstall.sh
+++ script/nodeinstall.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-sed -i '1i\192.168.1.221 kmaster01.lan kub-master01' /etc/hosts
-sed -i '2i\192.168.1.222 kmaster02.lan kub-master02' /etc/hosts
-sed -i '3i\192.168.1.223 knode03.lan knode03' /etc/hosts
-sed -i '4i\192.168.1.224 knode04.lan knode04' /etc/hosts
-sed -i '5i\192.168.1.225 knode05.lan knode05' /etc/hosts
+sed -i '1i\192.168.1.221 kmaster01.lan kmaster01' /etc/hosts
+sed -i '2i\192.168.1.222 kmaster02.lan kmaster02' /etc/hosts
+sed -i '3i\192.168.1.223 kmaster03.lan kmaster03' /etc/hosts
+sed -i '4i\192.168.1.224 kmaster04.lan kmaster04' /etc/hosts
+sed -i '5i\192.168.1.225 kmaster05.lan kmaster05' /etc/hosts
 echo "Hosts файл обновлен!!!!!!!"
-
+sed -i '1i\deb http://apt.kubernetes.io/ kubernetes-xenial main' /etc/apt/sources.list.d/kubernetes.list 
 apt update
 apt install -y docker.io
 apt update && apt install -y apt-transport-https curl 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 
-sed -i '1i\deb http://apt.kubernetes.io/ kubernetes-xenial main' /etc/apt/sources.list.d/kubernetes.list 
+
   
 apt update
 apt install -y kubelet kubeadm kubectl
