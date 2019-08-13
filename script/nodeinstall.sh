601473d284077372abbe2d6f730fa541b70b72bd
zcat7449 <komtehsnab@gmail.com>
2019-08-13 08:51:58 +0500

    Update nodeinstall.sh

--- script/nodeinstall.sh
+++ script/nodeinstall.sh
@@ -5,14 +5,10 @@ sed -i '2i\192.168.1.222 kmaster02.lan kmaster02' /etc/hosts
 sed -i '3i\192.168.1.223 kmaster03.lan kmaster03' /etc/hosts
 sed -i '4i\192.168.1.224 kmaster04.lan kmaster04' /etc/hosts
 sed -i '5i\192.168.1.225 kmaster05.lan kmaster05' /etc/hosts
-echo "Hosts файл обновлен!!!!!!!"
+echo "Hosts файл обновлен!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 > /etc/apt/sources.list.d/kubernetes.list
-sed -i '1i\deb http://apt.kubernetes.io/ kubernetes-xenial main' /etc/apt/sources.list.d/kubernetes.list 
-apt update
-apt install -y docker.io
+sed -i '1i\deb http://apt.kubernetes.io/ kubernetes-xenial main' /etc/apt/sources.list.d/kubernetes.list
 apt update && apt install -y apt-transport-https curl 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 
-
-  
 apt update
-apt install -y kubelet kubeadm kubectl
+apt install -y kubelet kubeadm kubectl docker.io 
