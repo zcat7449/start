a330bb8877c2c9434c569668f43961ae350cc2ef
zcat7449 <komtehsnab@gmail.com>
2019-08-13 09:05:28 +0500

    Update nodeinstall.sh

--- script/nodeinstall.sh
+++ script/nodeinstall.sh
@@ -6,7 +6,7 @@ sed -i '3i\192.168.1.223 kmaster03.lan kmaster03' /etc/hosts
 sed -i '4i\192.168.1.224 kmaster04.lan kmaster04' /etc/hosts
 sed -i '5i\192.168.1.225 kmaster05.lan kmaster05' /etc/hosts
 echo "Hosts файл обновлен!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-> /etc/apt/sources.list.d/kubernetes.list
+echo '#kubernetes' /etc/apt/sources.list.d/kubernetes.list
 sed -i '1i\deb http://apt.kubernetes.io/ kubernetes-xenial main' /etc/apt/sources.list.d/kubernetes.list
 apt update && apt install -y apt-transport-https curl 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 
