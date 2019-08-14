ddab6a77d3dc87cb7671d914c888ec46d335de6a
zcat7449 <komtehsnab@gmail.com>
2019-08-14 11:22:52 +0500

    Update 2nodeinstall.sh

--- script/2nodeinstall.sh
+++ script/2nodeinstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 
 echo "Hosts файл обновлен!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo '#kubernetes' > /etc/apt/sources.list.d/kubernetes.list
 sed -i '1i\deb http://apt.kubernetes.io/ kubernetes-xenial main' /etc/apt/sources.list.d/kubernetes.list
 apt update && apt install -y apt-transport-https curl 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 
 apt update
 apt install -y kubelet kubeadm kubectl docker.io docker-compose 
 systemctl enable docker
