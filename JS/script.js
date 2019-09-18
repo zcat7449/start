b80ad4d1a3f8c823a2172371c32f81ac19d8295d
Stas <komtehsnab@gmail.com>
2019-09-18 11:25:45 +0500

    latest

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// console.log(second);
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+var number=5;
+var string="Hello";
+
+//console.log(4/0);
+//console.log('string'*9);
+
+let something;
+console.log(something)
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone.name);
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.png','apple.bmp'];
+console.log(arr[0]);
+
+
+alert("Hello world!")
+
+
