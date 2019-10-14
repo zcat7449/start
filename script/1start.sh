e869fc452a9679a9b54be6c2de96fe8f9b35c7e8
Stas <komtehsnab@gmail.com>
2019-10-14 16:24:25 +0500

    Update 1start.sh

--- script/1start.sh
+++ script/1start.sh
@@ -3,18 +3,18 @@ echo "Start script"
 echo "Запуск обновления"
 apt -y install mc wget curl
 sed -i '32c\PermitRootLogin no' /etc/ssh/sshd_config
-sed -i '122i\Match Address 192.168.1.1' /etc/ssh/sshd_config
+sed -i '122i\Match Address 192.168.88.1' /etc/ssh/sshd_config
 sed -i '123i\	     PermitRootLogin no' /etc/ssh/sshd_config
-sed -i '124i\Match Address 192.168.1.0/24' /etc/ssh/sshd_config
+sed -i '124i\Match Address 192.168.88.0/24' /etc/ssh/sshd_config
 sed -i '125i\	     PermitRootLogin yes' /etc/ssh/sshd_config
 swapoff -a
 timedatectl set-timezone Asia/Yekaterinburg
 sed -i 's!/swap!#/swap!' /etc/fstab
-sed -i '1i\192.168.1.221 kmaster01 kmaster01.lan' /etc/hosts
-sed -i '2i\192.168.1.222 kmaster02 kmaster02.lan' /etc/hosts
-sed -i '3i\192.168.1.223 kmaster03 kmaster03.lan' /etc/hosts
-sed -i '4i\192.168.1.224 kmaster04 kmaster04.lan' /etc/hosts
-sed -i '5i\192.168.1.225 kmaster05 kmaster05.lan' /etc/hosts
+sed -i '1i\192.168.88.221 kmaster01 kmaster01.lan' /etc/hosts
+sed -i '2i\192.168.88.222 kmaster02 kmaster02.lan' /etc/hosts
+sed -i '3i\192.168.88.223 kmaster03 kmaster03.lan' /etc/hosts
+sed -i '4i\192.168.88.224 kmaster04 kmaster04.lan' /etc/hosts
+sed -i '5i\192.168.88.225 kmaster05 kmaster05.lan' /etc/hosts
 sed -i '6i\192.168.88.221 vmaster01' /etc/hosts
 sed -i '7i\192.168.88.222 vmaster02' /etc/hosts
 sed -i '8i\192.168.88.223 vmaster03' /etc/hosts
